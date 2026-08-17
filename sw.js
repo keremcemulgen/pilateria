24e6a4bb3a4ca7da589062aa8c97218c1a5fe2ba
keremcemulgen <keremcemoztepe@gmail.com>
2026-08-17 09:29:32 +0300

    v146: panel listesi olcutu duzeltildi (yapildi+yandi 7/8 veya 8/8)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // v51 KOK FIX: eski SW, Supabase GET okumalarini da cache'liyordu; ag kesintisinde ESKI veriyi servis edip
 // state'i geri sariyordu. Artik yalniz kendi kaynagimizdaki (app dosyalari) GET'ler yonetilir; Supabase/harici
 // istekler HIC dokunulmadan dogrudan aga gider (veri tazeligi ZORUNLU).
-const CACHE_NAME = 'pilateria-v145-2026-08-17-68';
+const CACHE_NAME = 'pilateria-v146-2026-08-17-69';
 const ASSETS = [
   './',
   './index.html',
